09476d7f6e131ccf88360bdfe132f7c101a3f2eb
Ivan Poluhin <ivan.poluhin.200407@gmail.com>
2024-04-13 19:03:51 +0300

    Adding navigation

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,9 +10,14 @@ import { products } from '../products';
 export class ProductListComponent {
   products = [...products];
 
-  share() {
-    window.alert('The product has been shared!');
+  share(product: any) {
+    window.alert(`The product ${product.name} has been shared!`);
   }
+
+  onNotify(product: any) {
+    window.alert(`You will be notified when the product ${product.name} goes on sale`);
+  }
+
 }
 
 
